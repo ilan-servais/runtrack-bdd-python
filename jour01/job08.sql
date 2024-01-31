76eb6d77a031a8697fa1911b6e3d347630fb5124
ilan-servais <145970490+ilan-servais@users.noreply.github.com>
2024-01-31 15:20:12 +0100

    fix
    fix name

---
+++ jour01/job08.sql
@@ -0,0 +1,2 @@
+-- Récupérer la liste des étudiants de moins de 18 ans
+SELECT * FROM etudiant WHERE age < 18;
---
+++ jour01/job09.sql
@@ -0,0 +1,2 @@
+-- Trier les âges des étudiants par ordre croissant
+SELECT * FROM etudiant ORDER BY age ASC;
